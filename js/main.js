128a9f51446e38990ab369ce21a6aa3f8d02dc6a
Laleh <laleh@Lilis-MacBook-Pro-2.local>
2017-12-31 13:17:14 -0800

    JS with functions

--- js/main.js
+++ js/main.js
@@ -2,27 +2,22 @@
 
 var cards = ['queen', 'queen', 'king', 'king'];
 
-// Create an array to store the cards in play
+
 var cardsInPlay = [];
 
-// Create a variable cardOne to represent the first card the user flipped
-var cardOne = cards[0];
-// Add card to array of cards that are in play
-cardsInPlay.push(cardOne);
-// Use console.log() to display the card that the user flipped
-console.log("User flipped " + cardOne);
-
-// Create a variable cardTwo to represent the second card the user flipped
-var cardTwo = cards[2];
-// Add card to array of cards that are in play
-cardsInPlay.push(cardTwo);
-// Use console.log() to display the card that the user flipped
-console.log("User flipped " + cardTwo);
 
 
-// Check to see if two cards have been played
-if (cardsInPlay.length === 2) {
 
+
+
+
+
+
+
+
+
+
+var checkForMatch = function () {
   // Check to see if two cards match and provide feedback to user
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
@@ -30,4 +25,34 @@ if (cardsInPlay.length === 2) {
     alert("Sorry, try again.");
 
   }
+};
+var flipCard = function(cardId){
+console.log("User flipped " + cards[cardId]);
+
+// Create a variable cardTwo to represent the second card the user flipped
+
+// Add card to array of cards that are in play
+
+// Use console.log() to display the card that the user flipped
+
+
+cardsInPlay.push(cards[cardId]);
+
+
+// Check to see if two cards have been played
+if (cardsInPlay.length === 2) {
+
+  // Check to see if two cards match and provide feedback to user
+    checkForMatch();
+
+  
 }
+};
+
+
+
+
+
+
+flipCard(0);
+flipCard(2);
